fff14240082d76bc3df11a36fb36c8703e4588fe
mohamedshaban463 <mshabanewes@gmail.com>
2025-08-17 17:03:21 +0300

    pca_analysis

--- notebooks/data.py
+++ notebooks/data.py
@@ -3,20 +3,22 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-def main():
-    heart_disease = fetch_ucirepo(id=45)
-    X = heart_disease.data.features
-    y = heart_disease.data.targets
+heart_disease = fetch_ucirepo(id=45)
+X = heart_disease.data.features
+y = heart_disease.data.targets
 
-    df = pd.DataFrame(X)
+df = pd.DataFrame(X)
 
 #Handling the missing values
-    df = df.fillna(df.mean())
+df = df.fillna(df.mean())
 
 #standardizing data
-    scaler = StandardScaler()
-    standardized_data = scaler.fit_transform(df)
-    standardized_df = pd.DataFrame(standardized_data, columns=df.columns)
+scaler = StandardScaler()
+standardized_data = scaler.fit_transform(df)
+standardized_df = pd.DataFrame(standardized_data, columns=df.columns)
+
+def main():
+    print("Heart Disease Dataset:")
 
 if __name__ == "__main__":
     main()
